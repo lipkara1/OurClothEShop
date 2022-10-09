159f9f1493d162c44b1ecbcd73c7e10d2fe97931
lipkara1 <giorgoslipka@gmail.com>
2022-10-09 17:42:45 +0300

    Initial commit

---
+++ src/main/java/com/example/ourclotheshop/OurClothEShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.ourclotheshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurClothEShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurClothEShopApplication.class, args);
+    }
+
+}
